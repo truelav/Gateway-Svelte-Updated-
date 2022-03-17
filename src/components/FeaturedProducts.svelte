43496e93a30893d34f502ef3dd13a8804c336129
truelav <madan.serghei@yahoo.com>
2022-03-17 09:45:16 -0700

    updated the gallery lifestyle and made featured products as a component

---
+++ src/components/FeaturedProducts.svelte
@@ -0,0 +1,21 @@
+<script>
+    import {dataProducts} from '../data/allProducts.json'
+    export let allProducts = dataProducts.filter((item) => item.featured === "yes")
+    import ProductTablet from './ProductTablet.svelte';
+</script>
+
+
+<section class="text-gray-600 body-font">
+  <div class="container px-5 py-24 mx-auto">
+    <div class="flex flex-wrap sm:-m-4 -mx-4 -mb-10 -mt-4">
+      {#each allProducts as item}
+        <ProductTablet {item}/>
+      {/each}
+    </div>
+  </div>
+</section>
+
+
+<style>
+
+</style>
